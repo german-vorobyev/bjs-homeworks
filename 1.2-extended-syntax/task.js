1e87e68000064742545ce6b49dd53fd56993b2ed
german-vorobyev <german.v93@mail.ru>
2020-08-12 14:26:07 +0300

    First commit

--- 1.2-extended-syntax/task.js
+++ 1.2-extended-syntax/task.js
@@ -1,17 +1,39 @@
 "use strict"
 function getResult(a,b,c){
-    // код для задачи №1 писать здесь
-    // return 
+    let D = b * b - 4 * a * c;
+    let x1,x2;
+    if (D > 0) {
+        x1 = (-b + Math.sqrt(D)) / (2 * a);
+        x2 = (-b - Math.sqrt(D)) / (2 * a);
+        return [x1,x2];
+    } else if (D == 0) {
+        x1 = -b / (2 * a);
+        return [x1];
+    } else if (D < 0) {
+        return [];
+    }
 }
-let result = getResult(2,3,4)
-
 
 function getAverageMark(marks){
-    // код для задачи №2 писать здесь
-    // return averageMark;
+    if (marks.length < 1) {
+        return 0;
+    }
+    let marksSum = 0;
+    for (let i = 0; i < marks.length; i++) {
+        if (marks.length > 5) {
+            marks.splice(5);
+        }
+    marksSum += marks[i];
+    }
+    let averageMark = marksSum / marks.length;
+
+    return averageMark;
 }
 
 function askDrink(name,dateOfBirthday){
-    // код для задачи №3 писать здесь
-    // return result;
+    if (dateOfBirthday.getFullYear() < (new Date().getFullYear() - 18)) {
+        return `Не желаете ли олд-фэшн, ${name}?`;
+    } else {
+        return `Сожалею, ${name}, но я не могу вам продать алкоголь. Зато могу предложить вам замечательный клюквенный компот!`;   
+    }
 }
